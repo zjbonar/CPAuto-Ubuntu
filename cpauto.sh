6476bcc43755d92a8b11299fbbf43ac0065897ff
zjbonar <zjbonar@gmail.com>
2016-11-29 09:45:13 -0800

    Update cpauto.sh

--- cpauto.sh
+++ cpauto.sh
@@ -1,6 +1,8 @@
- #!/bin/bash
- clear
- echo "Remember to run as root"
- echo "Running CPAuto..."
- ufw enable
- echo "Script Completed"
+#!/bin/bash
+clear
+echo "Remember to run as root"
+echo "Running CPAuto at $(date +%T)"
+echo "Enabling Firewall..."
+ufw enable
+sleep 3s
+echo "Script completed at $(date +%T)"
